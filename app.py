fd6d42510f82a0def112e9812615e1d881bfcedf
erwin <erwin.stein@gmail.com>
2023-04-13 16:51:47 -0300

    13/04/23 : 16-50

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 import sqlalchemy
 from forms import *
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ÇALDKFJAÇALDKJFALFJ'
 
 @app.route('/', methods=['GET','POST'])
 def login():
-	return render_template('login.html', form=FormLogin())
+    form = FormLogin(request.form)
+    if request.method == 'POST' and form.validate():
+        if form.email.data.strip() == 'erwin.stein@gmail.com' and form.senha.data.strip() == 'erwinstein':
+            return render_template('comandas.html')    
+        else:
+            return 'ERROR'   
+    return render_template('login.html', form=form)
+
+@app.route('/comandas.html', methods=['GET','POST'])
+def comandas():
+    return render_template('comandas.html')
+
 
 
 if __name__ == '__main__':
-    app.run(host='10.0.0.53', debug=True)
+    # app.run(host='192.168.0.216',debug=True)
+    app.run(host='10.0.0.53',debug=True)
--- forms.py
+++ forms.py
@@ -1,4 +1,19 @@
-from wtforms import Form, StringField
+from wtforms import Form, StringField, EmailField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms.validators import  ValidationError, Email, InputRequired, Length
 
 class FormLogin(Form):
-	usuario = StringField('Usuário') 
+	email = EmailField(
+		'Email',
+		validators = [
+		InputRequired(message=('Preencher este campo!')),
+		Email(message=('Digite um email válido!')),
+		])
+	senha = StringField(
+		'Senha',
+		validators = [
+		InputRequired(message=('Preencher este campo!')),
+		Length(min=6,max=20, message=('Mínimo 6 caracteres!'))
+		])
+
+	enviar = SubmitField('Entrar')
