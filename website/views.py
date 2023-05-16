7fc898c036255e7960dbaba4db16c7d8a19bb895
erwin <erwin.stein@gmail.com>
2023-05-16 10:25:18 -0300

    16/05/23 - 10:25

--- website/views.py
+++ website/views.py
@@ -21,12 +21,6 @@ views = Blueprint('views', __name__)
 @login_required
 def comandas():
     return render_template("comandas.html", user=current_user, data=data)
-# ***********************************************************************************************
-# DELETE NOTE
-# ***********************************************************************************************
-@views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    pass
 
 # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX    
 # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 
@@ -187,14 +181,12 @@ def deletarCliente(id):
     if id == 1:
         flash('NÃO PODE SER EXCLUÍDO!',category='error')
     else:
-        print(get_cli)
         if get_cli:
             db.session.delete(get_cli)
             db.session.commit()
             return redirect(url_for('views.clientes'))
 
     return redirect(url_for('views.clientes'))
-
 # ***********************************************************************************************
 # ATUALIZAR CLIENTES
 # ***********************************************************************************************
@@ -204,49 +196,42 @@ def atualizarClientes(id):
     form = CadastroCliente()
     get_cli = Cliente.query.get(id)
 
-    if id != 1:
-        if request.method == 'GET': 
+    if request.method == 'GET':
+        if id != 1:
             return render_template('clientes_atualizar.html', 
                 user=current_user, 
                 data=data, 
                 form=form,
                 c=get_cli
                 )
-
+        else:
+            flash('NÃO PODE SER EDITADO!')
+            return redirect(url_for('views.clientes'))
+    
+    if form.validate_on_submit():
         if request.method == 'POST':
-            if form.validate_on_submit():
-                nome = request.form.get('nome').upper()
-                fone = request.form.get('fone')
-                existis_fone = Cliente.query.filter_by(fone=fone).first()
-
-                if existis_fone == None:
-                    get_cli.nome = nome
-                    get_cli.fone = fone
-                    db.session.commit()
-                    return redirect(url_for('views.clientes'))
-                else:
-                    if existis_fone.id == id:
-                        get_cli.nome = nome
-                        get_cli.fone = fone
-                        db.session.commit()
-                        return redirect(url_for('views.clientes'))
-                    else:    
-                        flash('CELULAR JÁ CADASTRADO !!!', category='error')
-                        return render_template('clientes_atualizar.html', 
-                            user=current_user, 
-                            data=data, 
-                            form=form,
-                            c=get_cli
-                            )
+            nome = request.form.get('nome').upper()
+            fone = request.form.get('fone')
+            existis_fone = Cliente.query.filter_by(fone=fone).first()
+            if existis_fone == None or existis_fone.id == id:
+                get_cli.nome = nome
+                get_cli.fone = fone
+                db.session.commit()
             else:
+                flash('CELULAR JÁ CADASTRADO !!!', category='error')
                 return render_template('clientes_atualizar.html', 
                     user=current_user, 
                     data=data, 
                     form=form,
                     c=get_cli
-                    )
-
-        return redirect(url_for('views.clientes'))
+                    )  
     else:
-        flash('NÃO PODE SER EDITADO!')
-        return redirect(url_for('views.clientes'))
+        return render_template('clientes_atualizar.html', 
+            user=current_user, 
+            data=data, 
+            form=form,
+            c=get_cli
+            )
+
+    return redirect(url_for('views.clientes'))
+
