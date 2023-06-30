70c8afe13f341721cdefd29b6c7d4e9e8d1431e8
erwin <erwin.stein@gmail.com>
2023-06-30 14:11:14 -0300

    30/06/2023 - 14:14

--- commit-git.py
+++ commit-git.py
@@ -1,9 +1,7 @@
 import os
 import datetime 
 
+path = 'cd /home/erwin-hc/Documentos/schmithausen && pwd'
 commit = str(datetime.datetime.now().strftime("%d/%m/%Y - %H:%S "))
-gitu = f'git add . && git commit -m "{commit}" && git push'        
-
-os.system('cd /home/erwin-hc/Documentos/schmithausen')
-os.system('pwd')
+gitu = f'{path} && git add . && git commit -m "{commit}" && git push'        
 os.system(gitu)
